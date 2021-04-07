67180d72f7e01b53e1efc772a36b358ac4d1b7ba
cyhang <1433452606@qq.com>
2021-04-07 20:59:29 +0800

    1

---
+++ Utils/Controller.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+class Controller : MonoBehaviour
+{
+    public bool createTreeToggle;
+    public Material ShadowMaterial;
+    public Texture tex;
+    public GameObject sceneAABB;
+    public Light light;
+
+    void Start()
+    {
+        if (light != null)
+        {
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
+
